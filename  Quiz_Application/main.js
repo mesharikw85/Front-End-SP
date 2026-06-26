7b4fa8a17cff0457e35480880fd276cfd55f791b
mesharikw85 <mkwg85@gmail.com>
2026-06-26 17:38:24 +0300

    Add Data Function p1

---  Quiz_Application/main.js
+++  Quiz_Application/main.js
@@ -1,24 +1,30 @@
 // Select Elemnts
 let countSpan = document.querySelector(".count span");
 let bulletsSpanContainer = document.querySelector(".bullets .spans");
+let quizArea = document.querySelector(".quiz-area");
 
+// Set Option
+let currentIndex = 0;
 function getQuestions() {
   let myRequest = new XMLHttpRequest();
 
   myRequest.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       let questionsObj = JSON.parse(this.responseText);
-      let questionsCount = questionsObj.length;
-      console.log(questionsCount);
+      let qCount = questionsObj.length;
+      console.log(qCount);
       // Create Bullets + Set Questions Count
-      createBullets(questionsCount);
+      createBullets(qCount);
+
+      // Add Question Data
+      addQuestionData(questionsObj[currentIndex], qCount);
     }
   };
 
   myRequest.open("Get", "html_questions.json", true);
   myRequest.send();
 }
-// Create The Bullets Function
+// Add Data Function p1
 getQuestions();
 
 function createBullets(num) {
@@ -28,11 +34,39 @@ function createBullets(num) {
   for (let i = 0; i < num; i++) {
     //Create Bullet
     let theBullet = document.createElement("span");
-    // Check if ts first span 
+    // Check if ts first span
     if (i === 0) {
       theBullet.className = "on";
     }
     // Append Bullet To Main Container
     bulletsSpanContainer.appendChild(theBullet);
+  }
+}
+
+function addQuestionData(obj, count) {
+  console.log(obj);
+  console.log(count);
+  // Create H2 Question
+  let questionTitle = document.createElement("h2");
+  // Create Question Text
+  // let questionText = document.createTextNode(obj.title);
+  let questionText = document.createTextNode(obj["title"]);
+  // Append Text To H2
+  questionTitle.append(questionText);
+  // Append H2 To Quiz Area
+  quizArea.append(questionText);
+  // Create The Answer
+  for (let i = 1; i <= 4; i++) {
+    //create Main Answer Div
+    let mainDiv = document.createElement("div");
+    // Add Class To Main Div
+    mainDiv.className = "answer";
+    // Create Radio Input
+    let radioInput = document.createElement("input");
+    // Add Type + Name + Id + Data-Atrribute
+    radioInput.name = "question";
+    radioInput.type = "radio ";
+    radioInput.id = `answer_${i}`;
+    radioInput.dataset.answer = obj[`answer_${i}`];
   }
 }
