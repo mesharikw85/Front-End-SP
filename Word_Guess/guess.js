d6aae8958270dc0a774c0d09d122cf1ab1e9b822
mesharikw85 <mkwg85@gmail.com>
2026-07-04 22:02:26 +0300

    Part3 Manage Navigations

--- Word_Guess/guess.js
+++ Word_Guess/guess.js
@@ -5,7 +5,7 @@ document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML =
   `${gameName} Game Created By Meshari`;
 
-// Part 2 Generate Inputs
+// Part3 Manage Navigations
 
 // Setting Game Options
 let numberOfTries = 6;
@@ -22,7 +22,7 @@ function generateInput() {
 
     if (i != 1) tryDiv.classList.add("disabled-inputs");
 
-    for (let j = 1; j < numberOfTries; j++) {
+    for (let j = 1; j <= numberOfLetters; j++) {
       const inputs = document.createElement("input");
       inputs.type = "text";
       inputs.id = `guess-${i}-letter-${j}`;
@@ -32,7 +32,39 @@ function generateInput() {
 
     inputsContainer.append(tryDiv);
   }
+  // Focus On First Input In First Try Element
   inputsContainer.children[0].children[1].focus();
+
+  // Disable All Inputs Except First One
+  const inputsInDisabledDiv = document.querySelectorAll(
+    ".disabled-inputs input",
+  );
+  inputsInDisabledDiv.forEach((input) => (input.disabled = true));
+
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input, index) => {
+    input.addEventListener("input", function () {
+      // Convert Input To Uppercase
+      this.value = this.value.toUpperCase();
+      // console.log(index);
+      const nextIndex = inputs[index + 1];
+      if (nextIndex) nextIndex.focus();
+    });
+
+    input.addEventListener("keydown", function (event) {
+      // console.log(event);
+      const currentIndex = Array.from(inputs).indexOf(event.target);
+      console.log(currentIndex);
+      if (event.key === "ArrowRight") {
+        const nextInput = currentIndex + 1;
+        if (nextInput < inputs.length) inputs[nextInput].focus();
+      }
+      if (event.key === "ArrowLeft") {
+        const prevInput = currentIndex - 1;
+        if (prevInput >= 0) inputs[prevInput].focus();
+      }
+    });
+  });
 }
 
 window.onload = function () {
